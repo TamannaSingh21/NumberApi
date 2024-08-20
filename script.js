e3ffc55d04317702a5ef53fa29cb56059a09bb12
Tamanna Singh <111965122+TamannaSingh21@users.noreply.github.com>
2024-08-20 14:03:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-let input = document.getElementById('randomnumber');
-const display = document.getElementById('display');
-const proxy = 'https://cors-anywhere.herokuapp.com/';
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const targetUrl = `http://numbersapi.com/${number}/math`;
+const url = proxyUrl + targetUrl;
 
-input.addEventListener("input", function () {
-  getData();
-});
-
-function getData() {
-  let number = input.value;
-  let url = `${proxy}http://numbersapi.com/${number}/math`;
-
-  if (number !== '') {
-    fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.text();
-      })
-      .then(data => {
-        display.textContent = data;
-      })
-      .catch(error => {
-        display.textContent = "There was an error: " + error.message;
-        console.error('There was a problem with the fetch operation:', error);
-      });
-  }
-}
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.text();
+  })
+  .then(data => {
+    display.textContent = data;
+  })
+  .catch(error => {
+    display.textContent = "There was an error: " + error.message;
+    console.error('There was a problem with the fetch operation:', error);
+  });
